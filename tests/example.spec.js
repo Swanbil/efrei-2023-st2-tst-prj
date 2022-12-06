f5b6866e8b09e159545b8c06f6a1e2f7e4c964ac
Swanbil <swan.bilek@bbox.fr>
2022-12-06 15:58:59 +0100

    Test : create an employee

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,11 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('homepage has title and links to intro page', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('https://b.hr.dmerej.info/');
+});
 
+test('homepage has title and links to list employees page', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/HR DB/);
 
@@ -15,7 +17,44 @@ test('homepage has title and links to intro page', async ({ page }) => {
 
   // Click the get started link.
   await getStarted.click();
-  
+
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*employees/);
 });
+
+test('Create an employee', async ({ page }) => {
+  const pageNewemployee = page.getByRole('link', { name: 'Add new employee' });
+  await pageNewemployee.click();
+
+  const employee = {
+    name: "employee1",
+    email: "employee1@email.com",
+    address: "11 rue test",
+    city: "Tokyo",
+    zipCode: "11000",
+    hiringDate: "2000-05-25",
+    jobTitle: "Testor"
+  };
+  await page.getByPlaceholder('Name').fill(employee.name);
+
+  await page.getByPlaceholder('Email').fill(employee.email);
+
+  await page.locator('input[name=address_line1] >> visible=true').fill(employee.address);
+
+  await page.getByPlaceholder('Zip code').fill(employee.zipCode);
+
+  await page.getByPlaceholder('City').fill(employee.city);
+
+  await page.locator('input[name=hiring_date] >> visible=true').fill(employee.hiringDate);
+
+  await page.getByLabel('Job title').fill(employee.jobTitle);
+
+  await page.getByRole('button', { name: 'Add' }).click();
+
+  //return to list employee
+  await page.goto('https://b.hr.dmerej.info/employees');
+
+  //test if the list employee contain the new user
+  await expect(page.locator('table > tbody > tr').last()).toContainText(employee.name + " " + employee.email);
+
+})
